dd8a1993d3adb8d3beadc5d235348d1fddb39441
theoldzheng <theoldzheng@163.com>
2020-12-06 18:02:01 +0800

    12.6 第一次提交

---
+++ test/src/com/ChatAtTCP/Client.java
@@ -0,0 +1,24 @@
+package com.ChatAtTCP;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+    public static void main(String[] args) {
+        try {
+            InetAddress byAddress = InetAddress.getByName("127.0.0.1");
+            int port = 2020; //端口号
+            Socket socket = new Socket(byAddress,port);
+            OutputStream outputStream = socket.getOutputStream();
+            outputStream.write("这是一个测试内容！ This is a test.".getBytes());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
