f9b016f5fc39017e7bac4a9b8dce89cb02fcd6f0
theoldzheng <theoldzheng@163.com>
2021-01-16 20:43:22 +0800

    2021.1.16 完成面向对象中期的项目练习  加深理解 以及对 数组里面放对象的应用 接下来要迅速完成对面向对象的复习 巩固基础

---
+++ test/src/Day08/CustomerInformation/CMUtility/readKeyboard.java
@@ -0,0 +1,24 @@
+package Day08.CustomerInformation.CMUtility;
+
+import java.util.Scanner;
+
+public class readKeyboard {
+    private char inputChar;
+    private String inputString;
+    private int inputInt;
+
+    public readKeyboard() {
+    }
+
+    public char readChar() {
+        return new Scanner(System.in).next().charAt(0);
+    }
+
+    public String readString() {
+        return new Scanner(System.in).next();
+    }
+
+    public int readInt(){
+        return new Scanner(System.in).nextInt();
+    }
+}
---
+++ test/src/Day08/CustomerInformation/service/CustomerList.java
@@ -0,0 +1,84 @@
+package Day08.CustomerInformation.service;
+
+import Day08.CustomerInformation.bean.Customer;
+
+/**
+ * Customer的管理模块 内部用数组管理一组Customer对象 并提供相应的增删改查 和 遍历功能  供CustomerView调用
+ */
+
+
+public class CustomerList {
+    private Customer[] customers; //用于保存客户信息的数组
+    private int total = 0;  //记录客户的个数
+
+    public CustomerList() {
+    }
+
+    public Customer[] getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(Customer[] customers) {
+        this.customers = customers;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public CustomerList(int totalCustoms) {
+        this.customers = new Customer[totalCustoms];
+    }
+
+    //增加客户
+    public boolean addCustomer(Customer customer) {
+        if (total >= customers.length) {
+            return false;
+        }
+        customers[total++] = customer;
+        return true;
+
+    }
+
+    //修改客户
+    public boolean replaceCustomer(int index, Customer customer) {
+        if (index < 0 || index >= total) {
+            return false;
+        }
+        customers[index] = customer;
+        return true;
+    }
+
+    //删除客户
+    public boolean deleteCustomer(int index) {
+        if (index < 0 || index >= total) {
+            return false;
+        }
+        customers[index] = null;
+        total--;
+        return true;
+    }
+
+    //查询指定客户
+    public boolean findForIndex(int index) {
+        if (index < 0 || index >= total) {
+            return false;
+        }
+        System.out.println(customers[index].getName() + "   " + customers[index].getGender() + "   " + customers[index].getAge() + "   " + customers[index].getPhone() + "   " + customers[index].getEmail());
+        return true;
+    }
+
+    //查询全部客户
+    public boolean findAll() {
+        for (int i = 0; i < customers.length; i++) {
+            System.out.println(customers[i].getName() + "   " + customers[i].getGender() + "   " + customers[i].getAge() + "   " + customers[i].getPhone() + "   " + customers[i].getEmail());
+
+        }
+        return true;
+    }
+
+}
