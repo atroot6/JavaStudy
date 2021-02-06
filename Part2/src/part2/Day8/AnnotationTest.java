0f7060b27a4137a0a836bd2e1b3b0d254ffe64e2
theoldzheng <theoldzheng@163.com>
2021-02-06 23:33:41 +0800

    2021.2.6

--- Part2/src/part2/Day8/AnnotationTest.java
+++ Part2/src/part2/Day8/AnnotationTest.java
@@ -1,4 +1,18 @@
 package part2.Day8;
 
+import java.util.ArrayList;
+
+/**
+ *
+ */
+@Deprecated
 public class AnnotationTest {
+    public static void main(String[] args) {
+
+        @SuppressWarnings("unused")
+        int num = 19;
+
+        @SuppressWarnings({"rawtypes", "unused"})  //去除警告
+        ArrayList arrayList = new ArrayList();
+    }
 }
